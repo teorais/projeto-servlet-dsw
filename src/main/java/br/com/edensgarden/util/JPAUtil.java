af843be0762f49783919913e1e60b93b83bccb25
teorais <adaonetohti@hotmail.com>
2025-11-29 13:17:58 -0300

    Remoção de alguns comentários

--- src/main/java/br/com/edensgarden/util/JPAUtil.java
+++ src/main/java/br/com/edensgarden/util/JPAUtil.java
@@ -6,7 +6,6 @@ import javax.persistence.Persistence;
 
 public class JPAUtil {
 
-    // O nome "projeto-dsw-pu" DEVE ser igual ao que colocamos no persistence.xml
     private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("projeto-dsw-pu");
 
     public static EntityManager getEntityManager() {
